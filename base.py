88ff3bf20feda0a9d7dc41baedbe234b87cf7899
Mathilde <matoch24@live.fr>
2018-02-12 14:32:28 +0100

    v2

--- base.py
+++ base.py
@@ -8,7 +8,7 @@ VERT=(0,255,0)
 BLEU=(0,0,255)
 JAUNE=(255,255,0)
 colors=[ROUGE,VERT,BLEU,JAUNE]
-
+horloge=pygame.time.Clock()
 
 
 
@@ -17,17 +17,17 @@ fenetre=pygame.display.set_mode((640,480))
 
 
 pygame.draw.circle(fenetre,(ROUGE),[200,300],60,5)
-#pygame.draw.line(fenetre, (0,255,0), [0, 0], [50,30], 5)
+
 pygame.draw.circle(fenetre,(255,255,255),[0,0],5,0) # exemple
-pygame.draw.circle(fenetre,(VERT),[320,400],10,0) # balle
+pygame.draw.circle(fenetre,(255,255,255),[320,400],10,0) # bille couleurs
 pygame.draw.circle(fenetre,(VERT),[320,450],10,0) # balle
 
-
-pygame.display.flip()
 
 
 continuer = 1
 while continuer:
+	horloge.tick(10)# 10 fois par seconde
+	pygame.display.flip()
 	for event in pygame.event.get():   #On parcours la liste de tous les événements reçus
 		if event.type == QUIT:     #Si un de ces événements est de type QUIT
 			continuer = 0      #On arrête la boucle
